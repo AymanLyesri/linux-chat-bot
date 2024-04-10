19a8f52b9b2c7cce92428684ed0313af2553e084
AymanLyesri <lyesri99@gmail.com>
2024-04-10 20:23:53 +0000

    chat bot now works with gpu, time to finetune it

--- tinyLama.py
+++ tinyLama.py
@@ -1,28 +1,43 @@
-from transformers import pipeline
+import os
 import torch
-# Install transformers from source - only needed for versions <= v4.34
-# pip install git+https://github.com/huggingface/transformers.git
-# pip install accelerate
+from transformers import pipeline
+
+# Set the environment variable HSA_OVERRIDE_GFX_VERSION=10.3.0
+os.environ["HSA_OVERRIDE_GFX_VERSION"] = "10.3.0"
+
+# Check if CUDA (GPU support) is available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+
+# Load the text generation pipeline with PyTorch backend
+pipe = pipeline("text-generation",
+                model="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+                framework="pt",
+                device=device,
+                # max_length=200,  # Adjust the maximum length of the response
+                num_return_sequences=1)  # Adjust the number of returned sequences
 
 
-pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-                torch_dtype=torch.bfloat16, device_map="auto")
+def chat_bot(prompt):
+    # We use the tokenizer's chat template to format each message
+    messages = [
+        {
+            "role": "system",
+            "content": "You will talk as the first person",
+        },
+        {"role": "user", "content": prompt},
+    ]
+    prompt = pipe.tokenizer.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True)
+    outputs = pipe(prompt)
+    return outputs
+    # return outputs[0]["generated_text"]
 
-# We use the tokenizer's chat template to format each message - see https://huggingface.co/docs/transformers/main/en/chat_templating
-messages = [
-    {
-        "role": "system",
-        "content": "You are a friendly chatbot who always responds in the style of a pirate",
-    },
-    {"role": "user", "content": "How many helicopters can a human eat in one sitting?"},
-]
-prompt = pipe.tokenizer.apply_chat_template(
-    messages, tokenize=False, add_generation_prompt=True)
-outputs = pipe(prompt, max_new_tokens=256, do_sample=True,
-               temperature=0.7, top_k=50, top_p=0.95)
-print(outputs[0]["generated_text"])
-#
-# You are a friendly chatbot who always responds in the style of a pirate.</s>
-#
-# How many helicopters can a human eat in one sitting?</s>
-#
+
+# Chat loop
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == 'quit':
+        break
+    response = chat_bot(user_input)
+    print("Bot:", response)
